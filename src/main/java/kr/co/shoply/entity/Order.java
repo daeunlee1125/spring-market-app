438e8543788c523b70c64755ed69170110386720
jinwon1998jang <jinwonj96@naver.com>
2025-10-02 11:40:34 +0900

    modify order,orderitem(dto,entity)

--- src/main/java/kr/co/shoply/entity/Order.java
+++ src/main/java/kr/co/shoply/entity/Order.java
@@ -18,34 +18,25 @@ import java.time.LocalDateTime;
 @Table(name = "`order`") // Oracle 예약어 처리
 public class Order {
     @Id
-    @Column(name = "ord_no", length = 11)
-    private String ordNo;
 
-    @Column(name = "mem_id", length = 30, nullable = false)
-    private String memId;
+    private String ord_No;
 
-    @Column(name = "ord_name", length = 30)
-    private String ordName;
+    private String mem_Id;
 
-    @Column(name = "ord_hp", length = 13)
-    private String ordHp;
+    private String ord_Name;
 
-    @Column(name = "ord_zip", length = 30)
-    private String ordZip;
+    private String ord_Hp;
 
-    @Column(name = "ord_addr1", length = 50)
-    private String ordAddr1;
+    private String ord_Zip;
 
-    @Column(name = "ord_addr2", length = 50)
-    private String ordAddr2;
+    private String ord_Addr1;
 
-    @Column(name = "ord_total")
-    private Integer ordTotal;
+    private String ord_Addr2;
+
+    private Integer ord_Total;
 
     @CreationTimestamp // 객체 생성 시 현재 시간 자동 입력
-    @Column(name = "ord_date", nullable = false)
-    private LocalDateTime ordDate;
+    private LocalDateTime ord_Date;
 
-    @Column(name = "ord_payment", length = 50)
-    private String ordPayment;
+    private String ord_Payment;
 }

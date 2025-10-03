4dee7d94c936ceba7a0ba2bbcd6bdae9a706367e
daeunlee1125 <130773761+daeunlee1125@users.noreply.github.com>
2025-10-03 16:15:28 +0900

    Revert "Jimniii"

--- src/main/java/kr/co/shoply/controller/CouponController.java
+++ src/main/java/kr/co/shoply/controller/CouponController.java
@@ -1,68 +1,32 @@
 package kr.co.shoply.controller;
 
-import kr.co.shoply.dto.PageRequestDTO;
-import kr.co.shoply.dto.PageResponseDTO;
-import kr.co.shoply.dto.SysCouponDTO;
+
 import kr.co.shoply.service.CouponService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Slf4j
 @RequiredArgsConstructor
 @Controller
 public class CouponController {
 
+
     private final CouponService couponService;
 
+
     @GetMapping("admin/coupon/list")
-    public String list(PageRequestDTO pageRequestDTO, Model model) {
-        PageResponseDTO<SysCouponDTO> pageResponse = couponService.selectCouponList(pageRequestDTO);
-        model.addAttribute("pageResponse", pageResponse);
+    public String list(){
+
         return "admin/coupon/list";
     }
 
-
-    // list에서 쿠폰 종료 확인하는거
-    @ResponseBody
-    @PostMapping("/admin/coupon/end")
-    public String endCoupon(@RequestParam("cp_code") String cpCode) {
-        couponService.endCoupon(cpCode); // 서비스에서 DB update
-        return "success";
-    }
-
-
-    // 쿠폰 등록 처리
-    @ResponseBody
-    @PostMapping("/admin/coupon/register")
-    public String registerCoupon(@ModelAttribute SysCouponDTO couponDTO) {
-        log.info("쿠폰 등록 요청: {}", couponDTO);
-
-        try {
-            couponService.registerCoupon(couponDTO);
-            return "success";
-        } catch (Exception e) {
-            log.error("쿠폰 등록 실패", e);
-            return "fail";
-        }
-    }
-
-
-
-
-
-
-
-
     @GetMapping("admin/coupon/issued")
-    public String issued() {
+    public String issued(){
+
         return "admin/coupon/issued";
     }
-
-
-
-
 
 }
--- src/main/java/kr/co/shoply/mapper/CouponMapper.java
+++ src/main/java/kr/co/shoply/mapper/CouponMapper.java
@@ -1,25 +1,4 @@
 package kr.co.shoply.mapper;
 
-import kr.co.shoply.dto.PageRequestDTO;
-import kr.co.shoply.dto.SysCouponDTO;
-import org.apache.ibatis.annotations.Mapper;
-
-import java.util.List;
-
-@Mapper
 public interface CouponMapper {
-    // 쿠폰 목록 조회
-    List<SysCouponDTO> selectCouponList(PageRequestDTO pageRequestDTO);
-
-    // 쿠폰 총 개수
-    int selectCouponTotal(PageRequestDTO pageRequestDTO);
-
-    void updateCouponEnd(String cpCode);
-
-
-    int insertCoupon(SysCouponDTO couponDTO);
-
-
-
-
 }

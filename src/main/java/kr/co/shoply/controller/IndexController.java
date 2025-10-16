edc14122b6ff53d77bd2bd11f50e05a5a2df11c8
alswnstl23 <alswnstl23@gmail.com>
2025-10-16 19:10:44 +0900

    index 링크 ã수정

--- src/main/java/kr/co/shoply/controller/IndexController.java
+++ src/main/java/kr/co/shoply/controller/IndexController.java
@@ -1,12 +1,17 @@
 package kr.co.shoply.controller;
 
+import kr.co.shoply.dto.ProductDTO;
 import kr.co.shoply.service.IndexService;
 import kr.co.shoply.service.SiteInfoService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
@@ -21,6 +26,8 @@ public class IndexController {
         model.addAttribute("bestProducts", indexService.getBestProducts());
         model.addAttribute("discountProducts", indexService.getDiscountProducts());
         model.addAttribute("sidebarBestProducts", indexService.getSidebarBestProducts());
+        List<ProductDTO> productDTOs = indexService.getSidebarBestProducts();
+        log.info("productDTOs={}",productDTOs);
         return "index";
     }
 
---
+++ src/main/java/kr/co/shoply/controller/admin/cs/AdminQnaController.java
@@ -0,0 +1,4 @@
+package kr.co.shoply.controller.admin.cs;
+
+public class AdminQnaController {
+}

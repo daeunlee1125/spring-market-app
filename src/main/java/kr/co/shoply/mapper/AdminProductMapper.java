3556bca7ed4857f0e1fac796fac1d6971d857295
bisunesskyu <bisunesskyu@gmail.com>
2025-10-16 17:33:55 +0900

    상품목록 기초데이터  조회까지 완성

--- src/main/java/kr/co/shoply/mapper/AdminProductMapper.java
+++ src/main/java/kr/co/shoply/mapper/AdminProductMapper.java
@@ -1,13 +1,14 @@
 package kr.co.shoply.mapper;
 
-import kr.co.shoply.dto.ProFileDTO;
-import kr.co.shoply.dto.ProdNoticeDTO;
-import kr.co.shoply.dto.ProdOptionDTO;
-import kr.co.shoply.dto.ProductDTO;
+import kr.co.shoply.dto.*;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface AdminProductMapper {
+    List<ProductListDTO> selectProductList(@Param("memId") String memId, @Param("memLevel") int memLevel);
     int insertProduct(ProductDTO dto);
     int insertProductFile(ProFileDTO dto);
     int insertProductNotice(ProdNoticeDTO dto);
